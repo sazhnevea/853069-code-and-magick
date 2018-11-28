20c97b49708a3fd7bf1e757048e414e0bb1e4bae
sazhnevea <sazhnevea@gmail.com>
2018-11-28 15:39:36 +0400

    поправил замечания

--- js/setup.js
+++ js/setup.js
@@ -11,33 +11,35 @@ var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-var getRandomNumber = function (array) {
-  var randomNumber = Math.floor(Math.random() * array.length);
-  return randomNumber;
+var getRandomElement = function (array) {
+  var randomElement = Math.floor(Math.random() * array.length);
+  return randomElement;
 };
 
-var getSimularWizard = function (name, colorOne, colorTwo) {
-  var simularWizard = {};
-  simularWizard.name = firstNames[getRandomNumber(name)] + ' ' + secondNames[getRandomNumber(name)];
-  simularWizard.coatColor = colorOne[getRandomNumber(colorOne)];
-  simularWizard.eyesColor = colorTwo[getRandomNumber(colorTwo)];
-  return simularWizard;
+var getSimilarWizard = function (name, colorOne, colorTwo) {
+  var similarWizard = {};
+  similarWizard.name = firstNames[getRandomElement(name)] + ' ' + secondNames[getRandomElement(name)];
+  similarWizard.coatColor = colorOne[getRandomElement(colorOne)];
+  similarWizard.eyesColor = colorTwo[getRandomElement(colorTwo)];
+  return similarWizard;
 };
 
 // функция создания массива объектов волшебников
-var getSimularWizards = function (counter) {
-  var simularWizards = [];
-  for (var i = 1 - 1; i <= counter - 1; i++) {
-    simularWizards[i] = getSimularWizard(firstNames, coatColor, eyesColor);
+var getSimilarWizards = function (counter) {
+  var similarWizards = [];
+  for (var i = 0; i < counter; i++) {
+    similarWizards[i] = getSimilarWizard(firstNames, coatColor, eyesColor);
   }
-  return simularWizards;
+  return similarWizards;
 };
 
 // массив объектов волшебников
-var simularWizards = getSimularWizards(WIZARDS_COUNTER);
+var similarWizards = getSimilarWizards(WIZARDS_COUNTER);
 
 var similarListElement = userDialog.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+                            .content
+                            .querySelector('.setup-similar-item');
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -48,9 +50,11 @@ var renderWizard = function (wizard) {
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < simularWizards.length; i++) {
-  fragment.appendChild(renderWizard(simularWizards[i]));
+for (var i = 0; i < similarWizards.length; i++) {
+  fragment.appendChild(renderWizard(similarWizards[i]));
 }
 similarListElement.appendChild(fragment);
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+userDialog.querySelector('.setup-similar')
+.classList
+.remove('hidden');
